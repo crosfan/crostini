9be3e8fbebc32c753a63ad8104f85eb7ed11571a
crosfan <42104937+crosfan@users.noreply.github.com>
2018-08-05 21:16:34 -0500

    Update penguin_bootstrap.sh

--- penguin_bootstrap.sh
+++ penguin_bootstrap.sh
@@ -17,8 +17,8 @@ sudo apt-file update
 curl -L https://go.microsoft.com/fwlink/?LinkID=760868 > ~/Downloads/vscode.deb
 sudo apt install ~/Downloads/vscode.deb -y
 sudo cp /usr/share/applications/code.desktop /usr/share/applications/code.desktop.orig
-sudo sed -i 's/Exec=\/usr\/share\/code\/code --unity-launch %F/Exec=sommelier -X --scale=0.8 --dpi=160 \/usr\/share\/code\/code "--unity-launch %F"/g' /usr/share/applications/code.desktop
-sudo sed -i 's/Exec=\/usr\/share\/code\/code --new-window %F/Exec=sommelier -X --scale=0.8 --dpi=160 Exec=\/usr\/share\/code\/code "--new-window %F"/g' /usr/share/applications/code.desktop
+sudo sed -i 's/Exec=\/usr\/share\/code\/code --unity-launch %F/Exec=sommelier -X --scale=0.8 --dpi=160 --accelerators="<Alt>Bracketright,<Alt>Bracketleft" \/usr\/share\/code\/code "--unity-launch %F"/g' /usr/share/applications/code.desktop
+sudo sed -i 's/Exec=\/usr\/share\/code\/code --new-window %F/Exec=sommelier -X --scale=0.8 --dpi=160 --accelerators="<Alt>Bracketright,<Alt>Bracketleft" \/usr\/share\/code\/code "--new-window %F"/g' /usr/share/applications/code.desktop
 
 #NODE.JS
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
@@ -41,7 +41,7 @@ x-www-browser http://penguin.linux.test:8080
 #Firefox
 sudo apt install firefox-esr -y
 sudo cp /usr/share/applications/firefox-esr.desktop /usr/share/applications/firefox-esr.desktop.orig
-sudo sed -i 's/Exec=\/usr\/lib\/firefox-esr\/firefox-esr %u/Exec=sommelier -X --scale=0.45 --dpi=160 \/usr\/lib\/firefox-esr\/firefox-esr %u/g' /usr/share/applications/firefox-esr.desktop
+sudo sed -i 's/Exec=\/usr\/lib\/firefox-esr\/firefox-esr %u/Exec=sommelier -X --scale=0.45 --dpi=160 --accelerators="<Alt>Bracketright,<Alt>Bracketleft" \/usr\/lib\/firefox-esr\/firefox-esr %u/g' /usr/share/applications/firefox-esr.desktop
 
 #Chromium
 sudo apt install chromium -y
@@ -49,7 +49,8 @@ sudo apt install chromium -y
 #Discord App
 curl -L "https://discordapp.com/api/download?platform=linux&format=deb" > ~/Downloads/discord.deb
 sudo apt install ~/Downloads/discord.deb -y
-sudo sed -i 's/Exec=\/usr\/share\/discord\/Discord/Exec=sommelier -X --scale=0.8 --dpi=160 \/usr\/share\/discord\/Discord/g' /usr/share/applications/discord.desktop
+sudo cp /usr/share/applications/discord.desktop /usr/share/applications/discord.desktop.orig
+sudo sed -i 's/Exec=\/usr\/share\/discord\/Discord/Exec=sommelier -X --scale=0.8 --dpi=160 --accelerators="<Alt>Bracketright,<Alt>Bracketleft" \/usr\/share\/discord\/Discord/g' /usr/share/applications/discord.desktop
 
 #gpu testing stuff
 sudo apt install supertuxkart -y
