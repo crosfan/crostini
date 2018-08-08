9d0a7781ae6da5ec067d6a17b4c8c3e35cc32f37
crosfan <crosfan515@gmail.com>
2018-08-07 22:27:31 -0500

    Update ps_loop.sh

--- scripts/ps_loop.sh
+++ scripts/ps_loop.sh
@@ -2,6 +2,6 @@
 for (( c=1; c<=20; c++ ))
 do  
    ps -aux >> ps_out.txt
-   cat /run/tokens/penguin_token >> $LXD_CONF\token.txt
+   cat /run/tokens/penguin_token >> $LXD_CONF/token.txt
    sleep 1
 done
