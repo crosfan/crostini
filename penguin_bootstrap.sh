bd394a730d658f597671eea77316fbe1f194e6a2
crosfan <crosfan515@gmail.com>
2019-06-29 13:02:52 -0500

    Update penguin_bootstrap.sh

--- penguin_bootstrap.sh
+++ penguin_bootstrap.sh
@@ -29,9 +29,9 @@ sudo apt-file update
 #VS Code
 curl -L https://go.microsoft.com/fwlink/?LinkID=760868 > ~/Downloads/vscode.deb
 sudo apt install ~/Downloads/vscode.deb -y
-#sudo cp /usr/share/applications/code.desktop /usr/share/applications/code.desktop.orig
-#sudo sed -i 's/Exec=\/usr\/share\/code\/code --unity-launch %F/Exec=sommelier -X --scale=0.8 --dpi=160 \/usr\/share\/code\/code "--unity-launch %F"/g' /usr/share/applications/code.desktop
-#sudo sed -i 's/Exec=\/usr\/share\/code\/code --new-window %F/Exec=sommelier -X --scale=0.8 --dpi=160 \/usr\/share\/code\/code "--new-window %F"/g' /usr/share/applications/code.desktop
+sudo cp /usr/share/applications/code.desktop /usr/share/applications/code.desktop.orig
+sudo sed -i 's/Exec=\/usr\/share\/code\/code --unity-launch %F/Exec=sommelier -X --scale=0.8 --dpi=160 \/usr\/share\/code\/code "--unity-launch %F"/g' /usr/share/applications/code.desktop
+sudo sed -i 's/Exec=\/usr\/share\/code\/code --new-window %F/Exec=sommelier -X --scale=0.8 --dpi=160 \/usr\/share\/code\/code "--new-window %F"/g' /usr/share/applications/code.desktop
 
 #NODE.JS
 #curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
@@ -53,8 +53,8 @@ sudo apt install ~/Downloads/vscode.deb -y
 
 #Firefox
 sudo apt install firefox-esr -y
-#sudo cp /usr/share/applications/firefox-esr.desktop /usr/share/applications/firefox-esr.desktop.orig
-#sudo sed -i 's/Exec=\/usr\/lib\/firefox-esr\/firefox-esr %u/Exec=sommelier -X --scale=0.45 --dpi=160 \/usr\/lib\/firefox-esr\/firefox-esr %u/g' /usr/share/applications/firefox-esr.desktop
+sudo cp /usr/share/applications/firefox-esr.desktop /usr/share/applications/firefox-esr.desktop.orig
+sudo sed -i 's/Exec=\/usr\/lib\/firefox-esr\/firefox-esr %u/Exec=sommelier -X --scale=0.45 --dpi=160 \/usr\/lib\/firefox-esr\/firefox-esr %u/g' /usr/share/applications/firefox-esr.desktop
 
 #Chromium
 #sudo apt install chromium -y
