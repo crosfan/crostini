b5c15084d052da4ffc916b6896e247111252367f
crosfan <crosfan515@gmail.com>
2019-12-07 08:00:08 -0600

    Update penguin_bootstrap.sh

--- penguin_bootstrap.sh
+++ penguin_bootstrap.sh
@@ -27,11 +27,11 @@ sudo apt install fuse -y
 sudo apt-file update
 
 #VS Code
-curl -L https://go.microsoft.com/fwlink/?LinkID=760868 > ~/Downloads/vscode.deb
-sudo apt install ~/Downloads/vscode.deb -y
-sudo cp /usr/share/applications/code.desktop /usr/share/applications/code.desktop.orig
-sudo sed -i 's/Exec=\/usr\/share\/code\/code --unity-launch %F/Exec=sommelier -X --scale=0.8 --dpi=160 \/usr\/share\/code\/code "--unity-launch %F"/g' /usr/share/applications/code.desktop
-sudo sed -i 's/Exec=\/usr\/share\/code\/code --new-window %F/Exec=sommelier -X --scale=0.8 --dpi=160 \/usr\/share\/code\/code "--new-window %F"/g' /usr/share/applications/code.desktop
+#curl -L https://go.microsoft.com/fwlink/?LinkID=760868 > ~/Downloads/vscode.deb
+#sudo apt install ~/Downloads/vscode.deb -y
+#sudo cp /usr/share/applications/code.desktop /usr/share/applications/code.desktop.orig
+#sudo sed -i 's/Exec=\/usr\/share\/code\/code --unity-launch %F/Exec=sommelier -X --scale=0.8 --dpi=160 \/usr\/share\/code\/code "--unity-launch %F"/g' /usr/share/applications/code.desktop
+#sudo sed -i 's/Exec=\/usr\/share\/code\/code --new-window %F/Exec=sommelier -X --scale=0.8 --dpi=160 \/usr\/share\/code\/code "--new-window %F"/g' /usr/share/applications/code.desktop
 
 #Firefox
 sudo apt install firefox-esr -y
@@ -39,29 +39,29 @@ sudo cp /usr/share/applications/firefox-esr.desktop /usr/share/applications/fire
 sudo sed -i 's/Exec=\/usr\/lib\/firefox-esr\/firefox-esr %u/Exec=sommelier -X --scale=0.45 --dpi=160 \/usr\/lib\/firefox-esr\/firefox-esr %u/g' /usr/share/applications/firefox-esr.desktop
 
 #Chromium
-sudo apt install chromium -y
+#sudo apt install chromium -y
 
 #Apache
-sudo apt install apache2 -y
+#sudo apt install apache2 -y
 
 #NodeJs
-sudo apt install build-essential software-properties-common -y
-curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
-sudo apt install nodejs -y
+#sudo apt install build-essential software-properties-common -y
+#curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+#sudo apt install nodejs -y
 
 
 #PHP Modules
-sudo apt install php7.0 libapache2-mod-php7.0 php7.0-cli php7.0-mcrypt php7.0-intl php7.0-mysql php7.0-curl php7.0-gd php7.0-soap php7.0-xml php7.0-zip php7.0-mbstring -y
-sudo cp /etc/php/7.0/apache2/php.ini /etc/php/7.0/apache2/php.ini.orig
-sudo sed -i 's/;date.timezone =/date.timezone = UTC/g' /etc/php/7.0/apache2/php.ini
-sudo sed -i 's/memory_limit = 128M/memory_limit = 512M/g' /etc/php/7.0/apache2/php.ini
-sudo sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.0/apache2/php.ini
-sudo sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 100M/g' /etc/php/7.0/apache2/php.ini
-sudo sed -i 's/post_max_size = 8M/post_max_size = 100M/g' /etc/php/7.0/apache2/php.ini
+#sudo apt install php7.0 libapache2-mod-php7.0 php7.0-cli php7.0-mcrypt php7.0-intl php7.0-mysql php7.0-curl php7.0-gd php7.0-soap php7.0-xml php7.0-zip php7.0-mbstring -y
+#sudo cp /etc/php/7.0/apache2/php.ini /etc/php/7.0/apache2/php.ini.orig
+#sudo sed -i 's/;date.timezone =/date.timezone = UTC/g' /etc/php/7.0/apache2/php.ini
+#sudo sed -i 's/memory_limit = 128M/memory_limit = 512M/g' /etc/php/7.0/apache2/php.ini
+#sudo sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.0/apache2/php.ini
+#sudo sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 100M/g' /etc/php/7.0/apache2/php.ini
+#sudo sed -i 's/post_max_size = 8M/post_max_size = 100M/g' /etc/php/7.0/apache2/php.ini
 
 #MariaDb
-sudo apt install mariadb-server -y
-sudo mysql_secure_installation #Need to just script out the necessary commands that this script executes here instead of calling this
+#sudo apt install mariadb-server -y
+#sudo mysql_secure_installation #Need to just script out the necessary commands that this script executes here instead of calling this
 
 #Upgrade everything
 sudo apt update
