6b1c5acc46b0c9d8a5b4bcc33e7ce823215e2ef5
crosfan <crosfan515@gmail.com>
2024-06-14 15:28:15 -0500

    Update penguin_bootstrap_min.sh

--- penguin_bootstrap_min.sh
+++ penguin_bootstrap_min.sh
@@ -37,6 +37,8 @@ sudo apt install chromium -y
 
 #Webex Teams
 mkdir -p ~/.local/share
+curl -L https://binaries.webex.com/WebexDesktop-Ubuntu-Official-Package/Webex.deb > ~/Downloads/Webex.deb
+sudo apt install ~/Downloads/Webex.deb -y
 
 #Upgrade everything
 sudo apt update
