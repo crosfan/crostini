fbab3856d9cc91a65b392725e7fe8ed2a18d19c0
crosfan <crosfan515@gmail.com>
2024-06-14 15:16:37 -0500

    Update penguin_bootstrap_min.sh

--- penguin_bootstrap_min.sh
+++ penguin_bootstrap_min.sh
@@ -11,9 +11,9 @@ sudo apt install iputils-ping -y
 sudo apt install traceroute -y
 sudo apt install dnsutils -y
 sudo apt install apt-file -y
-sudo apt install fuse -y
-sudo apt install xz-utils -y
-sudo apt-get install gnupg2 -y
+#sudo apt install fuse -y
+#sudo apt install xz-utils -y
+#sudo apt install gnupg2 -y
 
 sudo apt-file update
 
@@ -21,14 +21,24 @@ sudo apt-file update
 curl -L https://go.microsoft.com/fwlink/?LinkID=760868 > ~/Downloads/vscode.deb
 sudo apt install ~/Downloads/vscode.deb -y
 
+#PowerShell
+#source /etc/os-release
+#curl -L https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb > ~/Downloads/packages-microsoft-prod.deb
+#sudo dpkg -i packages-microsoft-prod.deb
+#rm packages-microsoft-prod.deb
+#sudo apt update
+#sudo apt install powershell -y
+
 #Firefox
 sudo apt install firefox-esr -y
 
 #Chromium
 sudo apt install chromium -y
 
-#Needed for Webex Teams
+#Webex Teams
 mkdir -p ~/.local/share
+curl -L https://binaries.webex.com/WebexDesktop-Ubuntu-Official-Package/Webex.deb > ~/Downloads/Webex.deb
+sudo apt install ~/Downlaods/Webex.deb -y
 
 #Upgrade everything
 sudo apt update
