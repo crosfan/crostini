abb54b22fffbf48a1d1e68cb1a6b9df974e1f7c9
crosfan <crosfan515@gmail.com>
2024-07-06 16:40:56 -0500

    Delete scripts/ps_loop.sh

--- scripts/ps_loop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for (( c=1; c<=50; c++ ))
-do  
-   ps -aux >> ps_out.txt
-   cat /run/tokens/penguin_token >> $LXD_CONF/token.txt
-   printf "\n" >> $LXD_CONF/token.txt
-   sleep 1
-done
